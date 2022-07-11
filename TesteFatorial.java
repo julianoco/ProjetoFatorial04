4ee31e95f96dc0f2f7c17275435117ff498c6444
Juliano Coelho Gonçalves de Melo - Windows <juliano@yahoo.com>
2022-07-11 15:18:17 -0300

    Alteração do arquivo TesteFatorial.java

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,7 +1,9 @@
-class TesteFatorial {
+class  TesteFatorial {
+
 	public static void main(String[] args) {
 		Fatorial fat = new Fatorial();
 		System.out.println(fat.calcularFatorial(5));
 	}
+
 }
 	
