506243cfed370f4cb0233b495f100ecaad111d4e
Juliano Coelho Gonçalves de Melo - Linux <julianoco@yahoo.com>
2022-07-13 15:40:57 -0300

    Modificação de Fatorial.java no sistema Linux

--- Fatorial.java
+++ Fatorial.java
@@ -1,11 +1,14 @@
 public class Fatorial {  
-
+	//Arquivo java modificado
+	
+	
 	public int calcularFatorial(int n) {
-		int resultado = 1;
+		//int resultado = 1;
 		for (int i = 2; i<=n; i++)
 			resultado = i;
 		return resultado;
 	}
+
 
 }
 
