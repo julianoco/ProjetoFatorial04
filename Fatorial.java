a72651715ad7676f66b7bb6b510f5574d7a28406
Juliano Coelho Gonçalves de Melo - Linux <julianoco@yahoo.com>
2022-07-15 08:11:28 -0300

    Atualização feito do linux

--- Fatorial.java
+++ Fatorial.java
@@ -1,15 +1,15 @@
 public class Fatorial {  
 
-	//Arquivo java modificado333
-	
-	
+	//Arquivo java modificado333***444
+		
 	public int calcularFatorial(int n) {
 		//int resultado = 1;
 		for (int i = 2; i<=n; i++)
+		
 			resultado = i;
+			
 		return resultado;
 	}
-
 
 }
 
